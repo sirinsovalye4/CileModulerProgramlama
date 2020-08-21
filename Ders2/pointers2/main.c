0d5fc2ff112f577b18fdce870f99c6044035b4ac
Omer Dogu <doguuomer@gmail.com>
2020-08-21 15:10:40 +0300

    pointers2 olusturuldu

--- Ders2/pointers2/main.c
+++ Ders2/pointers2/main.c
@@ -4,5 +4,24 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+int deg = 0;
+int *pdeg;
+
+deg = 10;
+
+pdeg = &deg;
+printf("degiskenin ilk degeri : %d\n",deg);
+
+*pdeg = 20;
+printf("degiskenin son degeri : %d\n",deg);
+printf("pointerin degeri : %d\n",pdeg);
+
+deg=15;
+printf("pointerin gosterdigi yerin degeri : %d\n",*pdeg);
+
+pdeg = pdeg + 2;
+printf("pointerin yeni degeri : %d\n",pdeg);
+printf("pointerin gosterdigi yerin degeri : %d\n",*pdeg);
+
 	return 0;
 }
