f6a3d01a4199012554a58e839d691fe7e23b8ee3
Omer Dogu <doguuomer@gmail.com>
2020-08-21 14:48:54 +0300

    ilk ornek

--- Ders2/main.c
+++ Ders2/main.c
@@ -4,5 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
+	int degisken1; //degisken tanimlama
+	int *isaretci1; //isaretci tanimlama
+	
+	isaretci1 = &degisken1;
+	
+	printf("%d %d",isaretci1 ,degisken1);
+	
+	
 	return 0;
 }
